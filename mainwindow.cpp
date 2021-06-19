7f906400748bf234a46c9886a52527448b813fef
Bruno <bmouraz@live.com>
2021-06-19 13:47:34 -0300

    Version 4.1

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1022,7 +1022,7 @@ void MainWindow::on_actionLine_Style_Scatter_Color_triggered()
 void MainWindow::on_actionExport_File_triggered()
 {
     QString selectedFilter;
-    QString filename = QFileDialog::getSaveFileName(this,tr("Open File"),tr("/home/"),tr("CSV(;) files (*.csv);;XLSX files (*.xlsx)"), &selectedFilter);
+    QString filename = QFileDialog::getSaveFileName(this,tr("Open File"),tr("/home/"),tr("CSV (*.csv);;XLSX (*.xlsx)"), &selectedFilter);
 
     if (!filename.isEmpty())
     {
